0cd31b5ba8c47637f4fb92ca95dcb82971b404c0
SaronHailemariam <149344922+SaronHailemariam@users.noreply.github.com>
2023-12-09 23:29:01 -0800

    Update app.R

--- app.R
+++ app.R
@@ -11,19 +11,40 @@ overdose_df<-read.csv("NCHS_-_Drug_Poisoning_Mortality_by_County__United_States.
 
 
 #Page 1
-#__________________________________________________________
-ui<-navbarPage("Death by Drug Overdose", 
-              h3("Introduction")
-                 p("As drug overdose-related deaths continues to prevail in society it is now necessary more 
-                than ever to understand the basis of drug overdose related and implications on public health deaths
-                . Analysis of drug overdose-related deaths has important implications for the clinical use of drugs, 
-                detection procedures, and public policy. In the present day, prescribed opioid use,  is brought into a 
+
+ 
+      intro_panel<- fluidPage( 
+              h3("Introduction "),
+              p("As drug overdose related deaths continues to prevail in society it is now necessary more 
+                than ever to understand the basis of drug overdose realted and implications on public health deaths
+                . Analysis of drug overdose related deaths has important implications for the clinical use of drugs, 
+                detection procedure, and public policy. In the present day, prescribed opioid use,  is brought into a 
                 new light as emerging laws and studies question the rates and nessecaitys of its use. And is therefore 
-                a good starting point.")
+                a good starting point."),
+         
+        
+         br(),
+         h2("Our Methods"),
+         p("We looked at death rates from... in... this mannyt people have died from drug overdoes in themost
+         recent year, 2021. 
+        we will explore how this number compares to previous year. We will also investigate a possible cause that might not be obvous..  "),
+br(),
+p("By: "),
+p("Using:"),
+p("Sources:"))
+         
+ 
+  #__________________________________________________________
+
+
+ 
    
-#Page 2
+   
+   
+   
+#Page 2 The drug overdoe rates
 #______________________________________________________________________
-tabPanel("Explore the problem",
+problem_panel<- fluidPage("Explore the problem",
 sidebarLayout(
   sidebarPanel(
     selectInput( 
@@ -34,50 +55,83 @@ sidebarLayout(
     selectInput( 
       inputId = "year_death", 
       label= "Select a year to view overdose death rates",
-      choices= df$Year)
-  ),
+      choices= df$Year),
+ # htmlOutput(outputId = "yr"),
+  #htmlOutput(outputId = "st"),
+ # htmlOutput(outputId = "year"),
+  ))
   mainPanel(
     h3("Seeing is believing"),
     tabsetPanel(
       tabPanel("Death by Year", plotlyOutput(outputId = "year")),
-      tabPanel("Death by State" , plotlyOutput(outputId = "state")),
-      #tabPanel("State and Year", plotlyOutput(outputId = "state and year"))
-    )
+      tabPanel("Death by State" , plotlyOutput(outputId = "state"))))
+      #tabPanel("State and Year", plotlyOutput(outputId = "state and year"))))
     
-  )))
+    
 
-#by state, by year, by year and state , 
+
+#Page 3 Medicare rates, 
+#______________________________________________________________________
+  Medicare_panel<- fluidPage("Medicare",
+                             
+                             )
+
+
+#Page 4 Zoom into the 4 years 2018-2021 and compare prescription rates and death rates
+#exploring medicare as a possible cause 
+#______________________________________________________________________
+  Comapre_panel<- fluidPage("Exploring Medicare as the causitve agent",
+                             
+  ) 
+#Page 5 Conclusions Is drug overdose deaths actually a probelm? is it caused by medicare? does medicare contribute at alll?
+#_____________________________________________________
+  Conclusions<- fluidPage("Conclusion"
+                            
+  ) 
+
+
+
+#Server stuff 
 #___________________________________________________________
-
+ui<-navbarPage("Death by Drug Overdose",
+               tabPanel("About", intro_panel),
+               tabPanel("Explore the problem", problem_panel),
+               tabPanel("Medicare",  Medicare_panel),
+               tabPanel("Exploring Medicare as the causitve agent", Comapre_panel),
+               tabPanel("Conclusion", Conclusions)
+               )
+               
+               
+               
 
 server<-function(input, output){
-
-#For Page 1
-#__________________________________________________
-
-
-
- #For Page 2
-#__________________________________________________
+ 
 output$state <- renderPlotly({  
-    A<-ggplot(df, aes(x=State, y=reorder(mean_Model.based.Death.Rate), ))+
+    A<-ggplot(df, aes(x=State, y=mean_Model.based.Death.Rate))+
       geom_bar(stat="identity")+
-      labs(x="State", y= "Death Rate", )
+      labs(x="State", y= "Death Rate" )
    
     A<- ggplotly(A, tooltip = "text") 
     
     return(A)
     })
+
 output$year<- renderPlotly({  
-  A<-ggplot(df, aes(x=df$Year, y=df$mean_Model.based.Death.Rate, ))+
+  A<-ggplot(df, aes(x=Year, y=mean_Model.based.Death.Rate))+
     geom_bar(stat="identity")+
-    labs(x="Year ", y= "Death Rate", )
+    labs(x="Year ", y= "Death Rate" )
   A<- ggplotly(A, tooltip = "text")
   return(A)
-})
+} ) 
+#output$yr <- renderUI({
+#  return(rate_deaths_per_yr(df, input$year_death)) 
+#})
 
-  
-  }        
+#output$st <- renderUI({
+#  return(rate_deaths_per_state(df, input$state_death)) 
+#})
+}
+     
 
     
-shinyApp(ui,server)  
+shinyApp(ui,server) 
